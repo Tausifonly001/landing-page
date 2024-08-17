e0c07a1d64f49f0089a5877f6cf7882932d87a1e
Md Tausif Ali Ansari <168260403+Tausifonly001@users.noreply.github.com>
2024-08-17 17:04:53 +0530

    added project files

---
+++ cars-landing-page/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
